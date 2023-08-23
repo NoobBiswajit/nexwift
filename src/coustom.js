0e3f65e6d2ba473ae2963b691be376aceff0a46c
noob-biswajit <developerbiswajit51@gmail.com>
2023-08-23 17:19:54 +0600

    Solution section design

--- src/coustom.js
+++ src/coustom.js
@@ -8,3 +8,16 @@ var swiper = new Swiper(".heroSwiper", {
     disableOnInteraction: false,
   },
 });
+
+// ======testslider=========
+
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
         dred: "#F00",
         Nblue: "#02075d",
         rpigment: "#ED1520",
+        bubbles: "#E8F9FF",
+      },
+      boxShadow: {
+        smallS: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;",
       },
     },
   },
