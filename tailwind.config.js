57e20a581e9c11e3975bd2cd04f1440d5a506307
noob-biswajit <developerbiswajit51@gmail.com>
2023-08-23 12:26:27 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      rlw: "Raleway",
+    },
+    extend: {
+      colors: {
+        dred: "#F00",
+        Nblue: "#02075d",
+      },
+    },
+  },
+  plugins: [],
+};
